77f29db225ae090d971bece1b9f3cae82fbe8e9e
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-05-27 21:42:36 +0530

    Create 1bii.twostrings.sh

---
+++ 1bii.twostrings.sh
@@ -0,0 +1,16 @@
+ii) Write Shell Scripts for the Following: 
+b.	Comparison of two strings
+
+#comparing strings
+#!/bin/bash
+echo "enter the strings:"
+read str1 str2
+if [ $str1 = $str2 ]
+then
+    echo "str1 and str2 are the same"
+else if [ $str1 \> $str2 ]
+then echo "$str1 is greater than $str2"
+else
+    echo "$str2 is greater than $str1"
+fi
+fi
